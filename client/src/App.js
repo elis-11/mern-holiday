ede945c6c650ee2b07516e7edddc940ffde9e635
elis-11 <eliza.arzanukaeva@gmail.com>
2021-12-23 11:59:14 +0100

    create AuthPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
+import React from "react";
+
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
+import AuthPage from "./pages/AuthPage/AuthPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <AuthPage />
     </div>
   );
 }
--- client/src/pages/AuthPage/AuthPage.jsx
+++ client/src/pages/AuthPage/AuthPage.jsx
@@ -1,2 +1,58 @@
-// 24:41
-// 24:41
+import React from "react";
+
+import './AuthPage.scss'
+
+const AuthPage = () => {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="auth-page">
+          <h3>Autorization</h3>
+          <form className="form form-login">
+            <div className="row">
+              <div className="input-field col s12">
+                <input type="email" name="email" className="validate" />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="input-field col s12">
+                <input type="password" name="password" className="validate" />
+                <label htmlFor="password">Password</label>
+              </div>
+            </div>
+            <div className="row">
+              <button className="wawes-effect wawes-light btn grey">
+                Login
+              </button>
+              <a href="/" className="btn-outline btn-reg">
+                  Don't you have an account?
+              </a>
+            </div>
+          </form>
+          <h3>Registration</h3>
+          <form className="form form-login">
+            <div className="row">
+              <div className="input-field col s12">
+                <input type="email" name="email" className="validate" />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="input-field col s12">
+                <input type="password" name="password" className="validate" />
+                <label htmlFor="password">Password</label>
+              </div>
+            </div>
+            <div className="row">
+              <button className="wawes-effect wawes-light btn grey">
+                Registration
+              </button>
+              <a href="/" className="btn-outline btn-reg">
+                  Do you have an account?
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default AuthPage;
