a6cb28cf7ffa7fe6a4d278bbdd3fe9fc7189f679
elis-11 <eliza.arzanukaeva@gmail.com>
2021-12-24 09:35:10 +0100

    expres-validator

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,27 @@
 const { Router } = require("express");
 const router = Router();
 const User = require("../models/User");
+const { check, validationResult } = require("express-validator");
 
-router.post("/registration", async (req, res) => {
+
+router.post("/registration",  
+
+[
+  check('email', 'wrong email').isEmail(),
+  check('password', 'wrong password').isLength({min: 4})
+],
+
+async (req, res) => {
   try {
+
+const errors=validationResult(req)
+if(!errors.isEmpty()){
+  return res.status(400).json({
+    errors: errors.array(),
+    message: 'wrong data by registration'
+  })
+}
+
     const { email, password } = req.body;
 
     const isUsed = await User.findOne({ email });
@@ -21,8 +39,7 @@ router.post("/registration", async (req, res) => {
 
     await user.save();
 
-    res.status(201).json({ message: "User created!"})
-
+    res.status(201).json({ message: "User created!" });
   } catch (error) {
     console.log(error);
   }
