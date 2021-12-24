d1a24cf780f117597ede31dcbbcafc7da2c19abc
elis-11 <eliza.arzanukaeva@gmail.com>
2021-12-24 16:09:56 +0100

    LOGOUT

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
-import './Navbar.scss'
+import "./Navbar.scss";
 
 const Navbar = () => {
-    return (
-        <nav>
-        <div className="nav-wrapper navbar #9e9e9e grey">
-          <a href="/" className="brand-logo">MERN todo App</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            {/* <li><Link to="sass.html">Login</Link></li> */}
-            <li><a href="/">Login</a></li>
-          </ul>
-        </div>
-      </nav>
-    )
-}
+  const { logout, isLogin } = useContext(AuthContext);
 
-export default Navbar
+  return (
+    <nav>
+      <div className="nav-wrapper navbar #9e9e9e grey">
+        <a href="/" className="brand-logo">
+          MERN todo App
+        </a>
+        {isLogin 
+        ? <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a href="/" onClick={logout}>Logout</a>
+            </li>
+          </ul>
+        : <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a href="/">Login</a>
+            </li>
+          </ul>
+        }
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
